e2d007ce10f732fce0fc3296014579d23f6ff468
fluffyorang3 <fluffyorang3@gmail.com>
2024-07-05 15:17:50 +0200

    Add append function

--- linkedList.js
+++ linkedList.js
@@ -1,10 +1,59 @@
-function linkedList() {
-  return {};
-}
-
-function node(value = null, nextNode = null) {
+function node(value = null, next = null) {
   return {
     value,
-    nextNode,
+    next,
+  };
+}
+
+function linkedList() {
+  let head = null;
+  let size = 0;
+  return {
+    append: function (value) {
+      const newNode = node(value);
+      if (head === null) {
+        head = newNode(value);
+      } else {
+        let current = head;
+        while (current.next) {
+          current = current.next;
+        }
+        current.next = newNode();
+      }
+      size++;
+    },
+    prepend: function (value) {
+      const newNode = node(value);
+      head = newNode;
+
+      size++;
+    },
+    size: function () {
+      console.log(size);
+    },
+    head: function () {
+      if (head === null) {
+        console.log("There is no head");
+      } else {
+        console.log(head);
+      }
+    },
+    tail: function () {
+      if (head === null) {
+        console.log("There is no tail");
+      } else {
+        let current = head;
+        while (current.next) {
+          current = current.next;
+        }
+        console.log(current);
+      }
+    },
+    at: function (index) {
+      if (index > size || index < 0) {
+        console.log("Index is out of range");
+      }
+      for (let i = 0; i < index; i++) {}
+    },
   };
 }
