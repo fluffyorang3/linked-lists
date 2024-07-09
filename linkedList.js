d8b9fbb4cdb03e1eaf9abddf2c9e28feebb34342
fluffyorang3 <fluffyorang3@gmail.com>
2024-07-09 13:47:21 +0200

    Rework project

--- linkedList.js
+++ linkedList.js
@@ -8,52 +8,101 @@ function node(value = null, next = null) {
 function linkedList() {
   let head = null;
   let size = 0;
-  return {
-    append: function (value) {
-      const newNode = node(value);
-      if (head === null) {
-        head = newNode(value);
-      } else {
-        let current = head;
-        while (current.next) {
-          current = current.next;
-        }
-        current.next = newNode();
-      }
-      size++;
-    },
-    prepend: function (value) {
-      const newNode = node(value);
-      head = newNode;
 
+  function append(value) {
+    if (head === null) {
+      head = node(value);
       size++;
-    },
-    size: function () {
-      console.log(size);
-    },
-    head: function () {
-      if (head === null) {
-        console.log("There is no head");
-      } else {
-        console.log(head);
-      }
-    },
-    tail: function () {
-      if (head === null) {
-        console.log("There is no tail");
-      } else {
-        let current = head;
-        while (current.next) {
-          current = current.next;
-        }
-        console.log(current);
-      }
-    },
-    at: function (index) {
-      if (index > size || index < 0) {
-        console.log("Index is out of range");
-      }
-      for (let i = 0; i < index; i++) {}
-    },
+      return;
+    }
+    let current = head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = node(value);
+    size++;
+  }
+
+  function prepend(value) {
+    if (head === null) {
+      head = node(value);
+      size++;
+      return;
+    }
+    let current = node(value);
+    current.next = head;
+    head = current;
+    size++;
+  }
+
+  function getSize() {
+    return size;
+  }
+
+  function getHead() {
+    return head;
+  }
+  function getTail() {
+    if (head === null) {
+      return null;
+    }
+    let current = head;
+    while (current.next) {
+      current = current.next;
+    }
+    return current;
+  }
+
+  function at(index) {
+    if (index < 0 || index >= size) {
+      return null;
+    }
+
+    if (head === null) {
+      return null;
+    }
+    let current = head;
+    for (let i = 0; i < index; i++) {
+      current = current.next;
+    }
+    return current;
+  }
+
+  function pop() {
+    if (head === null) {
+      return null;
+    }
+    let current = head;
+    let previous = null;
+    while (current.next) {
+      previous = current;
+      current = current.next;
+    }
+    previous.next = null;
+  }
+  function find(value) {
+    if (head === null) {
+      return null;
+    }
+    let current = head;
+    while (current.value !== value) {
+      current = current.next;
+    }
+    if (current.value !== value) {
+      return null;
+    } else {
+      return current.value;
+    }
+  }
+
+  return {
+    append,
+    prepend,
+    getSize,
+    getHead,
+    getTail,
+    at,
+    pop,
+    find,
   };
 }
